491b8170f583d2ad4c50570dd5411714cb1bc1e7
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-17 09:21:40 +0545

    Day-1 Use State Learning

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleIncrease=()=>{
+    setCount((prev)=>prev+1)
+  }
+
+  const handleDecrease=()=>{
+    if(count==0){
+      setCount(0)
+    }
+    else{
+      setCount((prev)=>prev-1)
+    }
+  }
+
+
+  return (
+    <div>
+      <h1>The value of Counter is {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
+}
+
+export default App

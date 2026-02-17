165437932f936a48305f6163f2b3509036f9ced9
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-17 09:25:38 +0545

    Day-1 Learning Array Maps

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [array, setArray] = useState(['Apple','Mango','Banana'])
 
   const handleIncrease=()=>{
     setCount((prev)=>prev+1)
@@ -18,11 +19,25 @@ const App = () => {
 
 
   return (
+    <>
     <div>
       <h1>The value of Counter is {count}</h1>
       <button onClick={handleIncrease}>Increase</button>
       <button onClick={handleDecrease}>Decrease</button>
     </div>
+
+    <div>
+      <h2>Array and Shift</h2>
+      <ul>
+        {
+          array?.map((item,index)=>
+          <li>{item}</li>
+          )
+        }
+      </ul>
+    </div>
+    </>
+    
   )
 }
 

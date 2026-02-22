1233a399e90245f52f8331aa7b29d0d695472699
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-22 12:32:54 +0545

    Day-5 learning to Apply Navlink to All pages using array.map()

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import {NavLink,Link} from 'react-router-dom'
 
 const Navbar = () => {
+  const navItem=[
+    {title:'Home',path:'/'},
+    {title:'About',path:'/about'},
+    {title:'Contact',path:'/contact'},
+  ]
   return (
     <>
    <nav className='bg-blue-500 text-white'>
     <div className='flex justify-between items-center text-xl px-4.5 '>
         <h2 className='font-extrabold '>Coding</h2>
         <div className='flex justify-center items-center gap-18  px-10 font-bold my-4.5 '>
-            <NavLink to='/' className={({isActive})=>isActive?"text-white":"text-black"}>Home</NavLink>
-            <Link to='/about'>About</Link>
-            <Link to='/contact'>Contact</Link>
+          {
+            navItem.map((item)=>
+            <NavLink to={item.path} className={({isActive})=>isActive?'text-black':'text-white'}>{item.title}</NavLink>
+            )
+          }
         </div>
     </div>
    </nav>

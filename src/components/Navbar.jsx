22888c1658c0d9c145e1230b69994342ec4b9980
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-22 14:06:39 +0545

    Day-5 Navbar is fully Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
   return (
     <>
    <nav className='bg-blue-500 text-white'>
-    <div className='flex justify-between items-center text-xl px-4.5 '>
+    <div className='flex justify-between items-center text-xl px-6'>
         <h2 className='font-extrabold '>Coding</h2>
         <div className='flex justify-center items-center gap-18  px-10 font-bold my-4.5 '>
           {
             navItem.map((item)=>
-            <NavLink  key={item.path} to={item.path} className={({isActive})=>isActive?'text-black bg-white rounded px-3 py-1':'text-white'}>{item.title}</NavLink>
+            <NavLink  key={item.path} to={item.path} className={({isActive})=>isActive?'text-black bg-white rounded transition duration-300 px-3 py-1':'text-white px-3 py-1'}>{item.title}</NavLink>
             )
           }
         </div>

4770670698626e420a7e05125d978f0322320305
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-24 09:16:41 +0545

    Day-6 Learning Post Method of Backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,17 @@ const express=require('express')
 const app=express()
 
 
+app.use(express.json())
+
+app.post('/post',(req,res)=>{
+    const {name,password}=req.body
+    res.status(201).json({
+        mesaage:'Creadted  Successfully',
+        name,
+        password
+    })
+})
+
 app.listen(5000,()=>{
     console.log('Server is running on the port 5000')
 })

bbd8b192f411c1d03b5d4e063a3b11ceca8d6c6b
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-25 09:24:43 +0545

    Day-8 Learning to Connected to Database Successfully

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,17 @@
+require('dotenv').config()
 const express=require('express')
-
+const mongoose=require('mongoose')
+const dns=require('dns')
+dns.setServers(['8.8.8.8','8.8.4.4'])
 const app=express()
 
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log('Connected to Database Successfully')
+})
+.catch((error)=>{
+    console.log('Database Connection Error',error)
+})
 
 app.listen(3000,()=>{
     console.log('Sever is running on the port 3000')

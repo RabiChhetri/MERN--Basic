6ef64ff55cc0400ba2000a6c91bde70750e882d6
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-25 10:36:43 +0545

    Day-8 leaning How to Make a Fully Optimized Form using the the attributes like value,onchange and onSubmit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    phone:'',
+    password:''
+  })
+  const updateValue=(e)=>{
+    setFormData((prevState)=>(
+      {...prevState,[e.target.name]:e.target.value}
+    ))
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log('Form Submitted')
+  }
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <span>Name:</span>
-        <input type="text" name="name" placeholder='Enter your name'/>
+        <input type="text" name="name" placeholder='Enter your name' value={formData.name} onChange={updateValue}/>
         <br />
         <span>Email:</span>
-        <input type="email" name="email" placeholder='Enter your email'/>
+        <input type="email" name="email" placeholder='Enter your email'value={formData.email} onChange={updateValue}/>
         <br />
         <span>Phone:</span>
-        <input type="number" name="phone" placeholder='Enter your phone number'/>
+        <input type="number" name="phone" placeholder='Enter your phone number' value={formData.phone} onChange={updateValue}/>
         <br />
         <span>Password:</span>
-        <input type="password" name="name" placeholder='Enter your password'/>
+        <input type="text" name="password" placeholder='Enter your password' value={formData.password} onChange={updateValue}/>
         <br />
         <button>Submit</button>
       </form>

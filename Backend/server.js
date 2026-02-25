f98d71ff1c02f178464e65bbcbe40ee515a6c2ef
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-25 09:12:17 +0545

    Day-8 Learning to Start the Backend Server

--- Backend/server.js
+++ Backend/server.js
@@ -1,44 +1,8 @@
 const express=require('express')
-const cors=require('cors')
 
-const corsOption={
-    origin:['http://localhost:5173'],
-    methods:['GET','POST','PUT','DELETE'],
-    allowedHeaders:['Content-Type','Authorization'],
-    credentials:true
-}
 const app=express()
 
-app.use(cors(corsOption))
-app.use(express.json())
-
-app.post('/post',(req,res)=>{
-    const {name,password}=req.body
-    res.status(201).json({
-        mesaage:'Creadted  Successfully',
-        name,
-        password
-    })
-})
-app.get('/get',(req,res)=>{
-    res.status(200).json({
-        message:'Fetch sucessfully'
-    })
-})
-app.post('/send',(req,res)=>{
-    console.log(req.body)
-    const{name,email,phone,password}=req.body
-    res.status(201).json({
-        message:'User Created Sucessfully',
-        data:{
-            name,
-            email,
-            phone,
-            password
-        }
-    })
-})
 
 app.listen(3000,()=>{
-    console.log('Server is running on the port 3000')
+    console.log('Sever is running on the port 3000')
 })

74a7856e5d4bb8219e495eeab2e2d98849316472
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-26 09:20:01 +0545

    Day-9 Learning how to save the data send from Frontend to  Database

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,24 @@
+const userModel=require('../models/user.model')
 async function registerUser(req,res) {
-    console.log(req.body)
+   try {
+     const {name,email,phone,password}=req.body
+    const isUserAlreadyExists=await userModel.findOne({email:email})
+    if(isUserAlreadyExists){
+       return res.status(401).json({
+        message:'Email already exists'
+        })
+    }
+    const user=await userModel.create({
+        name,email,phone,password
+    })
+    res.status(201).json({
+        message:'User register Successfully',
+        user
+    })
+   } catch (error) {
+    console.log('Inetrnal Error',error)
+    res.status(500).json({message:'Server Error'})
+   }
     
 }
 

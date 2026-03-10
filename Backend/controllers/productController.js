9698f5f107f2f49e8a1b4e0686493dc6a1c93ff3
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-03-10 12:41:13 +0545

    Day-14 Prodct Upload Controller is Created Sucessfully

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,17 @@
+const productModel=require('../models/product.model')
+const uploadOnCloudinary=require('../config/cloudinary')
+
+async function productUpload(params) {
+    const {name,price}=req.body
+    if(!name || !price){
+        return res.status(403).json({
+            message:'name or price is missing'
+        })
+    }
+    console.log(req.file)
+    console.log(name,price)
+
+    const imageUrl=await uploadOnCloudinary(req.file.path)
+    console.log(imageUrl)
+}
+module.exports={productUpload}

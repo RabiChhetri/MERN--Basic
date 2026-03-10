6a7a8f43a28da84115f5a41ed4213dafad7d36e2
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-03-10 12:43:53 +0545

    Day-14 Connecting the Product Crontoller and Product Router

--- Backend/router/productRouter.js
+++ Backend/router/productRouter.js
@@ -1,9 +1,10 @@
 const express=require('express')
-const multerConfig=require('../config/multerConfig')
+const upload=require('../config/multerConfig')
 const authMiddlewares=require('../middlewares/auth.middleware')
+const productController=require('../controllers/productController')
 
 const router=express.Router()
 
-router.post('/create',authMiddlewares.authMiddleware)
+router.post('/create',upload.single('image'),authMiddlewares.authMiddleware,productController.productUpload)
 
 module.exports=router

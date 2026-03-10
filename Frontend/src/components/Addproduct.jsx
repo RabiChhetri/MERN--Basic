bb5d3bbc50da042f3c64ffa0dc9953c3a34aa2a5
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-03-10 13:49:19 +0545

    Day-14 ProdctFrom Creadted Successfully

--- Frontend/src/components/Addproduct.jsx
+++ Frontend/src/components/Addproduct.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 const Addproduct = () => {
-    
+    const [product, setProduct] = useState({
+        name:'',
+        price:''
+    })
+    const updateValue=(e)=>{
+        setProduct((prevState)=>({
+            ...prevState,[e.target.name]:e.target.value
+        }))
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log('Form Submitted')
+        console.log(product.name)
+    }
   return (
     <div className="add-product-container">
-        <form className="product-form">
+        <form className="product-form" onSubmit={handleSubmit}>
             <h2>Add New Product</h2>
-            <input type="text" name="name" placeholder="Enter a product name"/>
-            <input type="number" name="price" placeholder="Enter a price"/>
-            <input type="file"/>
+            <input type="text" name="name" placeholder="Enter a product name" value={product.name}  onChange={updateValue}/>
+            <input type="number" name="price" placeholder="Enter a price" value={product.price} onChange={updateValue}/>
+            <input type="file" name="image"/>
             <button type='submit'>Add Product</button>
         </form>
     </div>

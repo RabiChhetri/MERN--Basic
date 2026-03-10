0381c189f44f94764e28d27de734675241b18a69
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-03-10 11:49:46 +0545

    Day-14  Learning to Intergarte the Cloudinary to upload the image

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary=require('cloudinary').v2
+const fs=require('fs')
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:CLOUDNARY_SECRET_KEY
+})
+
+async function uploadOnCloudinary(filepath) {
+    try {
+        const uploadResult=await cloudinary.uploader.upload(filepath)
+        fs.unlinkSync(filepath)
+        return uploadResult.secure_url
+    } catch (error) {
+        if(fs.existsSync){
+            fs.unlinkSync(filepath)
+        }
+    }
+    throw new Error('Cloudinary Uplaod failed')
+}
+
+module.exports={uploadOnCloudinary}
--- Backend/config/multerConfig.js
+++ Backend/config/multerConfig.js
@@ -1,11 +1,13 @@
 const multer=require('multer')
+const path=require('path')
 
-const storage=multer.memoryStorage()
-
-const upload=multer({
-    storage:storage,
-    limits:{
-        fullSize:50*1024*1024
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"./public")
+    },
+    filename:(req,file,cb)=>{
+        cb(file.originalname)
     }
 })
+const upload=multer({storage})
 module.exports=upload

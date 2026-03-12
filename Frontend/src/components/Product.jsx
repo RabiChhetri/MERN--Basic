b620abed2fc314f978cbd818f3ba8d04afbd92ea
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-03-12 08:59:24 +0545

    Day-16 Cleaning soem un wanted code

--- Frontend/src/components/Product.jsx
+++ Frontend/src/components/Product.jsx
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductDetails } from "../Apis/productApi";
-import React from "react";
+import React, { useState } from "react";
 
 const Product = () => {
+
+  const [cart, setCart] = useState([])
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ["products"],
@@ -19,32 +21,27 @@ const Product = () => {
   }
 
   return (
-    <div className="product-page">
-
-      <h1 className="title">Our Products</h1>
-
-      <div className="product-container">
-
-        {data?.product?.map((item) => (
-          <div className="product-card" key={item._id}>
-
-            <img
-              src={item.imageUrl}
-              alt={item.name}
-            />
-
-            <h3>{item.name}</h3>
-
-            <p className="price">${item.price}</p>
-
-            <button className="cart-btn">Add to Cart</button>
-
-          </div>
-        ))}
-
+    <div className="shop-container">
+      {/* PRODUCT SECTION */}
+      <div className="product-page">
+        <h1 className="title">Our Products</h1>
+        <div className="product-container">
+          {data?.product?.map((item) => (
+            <div className="product-card" key={item._id}>
+              <img src={item.imageUrl} alt={item.name} />
+              <h3>{item.name}</h3>
+              <p className="price">${item.price}</p>
+              <button
+                className="cart-btn"
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-
     </div>
+
   );
 };
 
